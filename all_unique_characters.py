bf3b68519d07731edd9b7b798f884295588ee1d4
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-05-21 17:58:56 +0200

    all_unique_characters creation

--- all_unique_characters.py
+++ all_unique_characters.py
@@ -1,16 +1,15 @@
-'''
+"""
 _______________________________________________________________________________
 Implement an algorithm to determine if a string has all unique characters.
-What if you can not use additional data structures?
 _______________________________________________________________________________
 Notes:
 1. with hash map
 2. nxn
 3. sort and compare
 time complexity, memory
-O(n) =
+O(n^2)
 _______________________________________________________________________________
- '''
+"""
 
 
 def all_unique_characters(string):
@@ -29,14 +28,13 @@ def all_unique_characters(string):
 
 def all_unique_characters2(string):
     for char in string:
-        if string.count(char) > 1:
+        if string.count(char) > 1:      # count is O(n)
             return False
     return True
 
 
 def all_unique_characters3(string):
     string = ''.join(sorted(string))
-    alex = string[:-1]
     for i, char in enumerate(string[:-1]):
         if char == string[i + 1]:
             return False
@@ -44,17 +42,17 @@ def all_unique_characters3(string):
 
 
 if __name__ == '__main__':
-    strings = ['az obicham mach i boza',
+    strings = ['I love playing rugby',
                'superb',
-               'Kakv0 st@va?',
-               'уникат',
-               'уникатт'
+               'How are you?',
+               'uniQ',
+               'not unique'
                ]
     print(strings)
 
     print('\n 1. with hash map')
-    print([*map(all_unique_characters, strings)])
+    print([*map(all_unique_characters, strings)])   # the star is used for unpacking
     print('\n 2. nxn')
     print([*map(all_unique_characters2, strings)])
-    print('\n 3. sort and copmare')
+    print('\n 3. sort and compare')
     print([*map(all_unique_characters3, strings)])
